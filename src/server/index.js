e2306d98f1f58d56bb8da320ccd8c5e1181273e8
luizschiestl <luizschiestl@gmail.com>
2019-12-13 15:39:30 -0300

    changed favicon and title

--- src/server/index.js
+++ src/server/index.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "../../build")));
 
-// app.get("*", (req, res) => {
-//     res.sendFile(__dirname + "../../build/index.html");
-// });
-
 io.on("connection", socket => {
     socket.on("savedDrawing", save => {
         io.sockets.emit("savedDrawing", save);
